80e37f57c3508ddca9aa527301990fc995dfe8c1
Lewis Maina Kabui <lewis_maina@yahoo.com>
2016-01-07 21:53:56 +0300

    Create "authenticated or read only" permissions

--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,17 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 
 class SnippetList(generics.ListCreateAPIView):
 	queryset = Snippet.objects.all()
 	serializers_class = SnippetSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Snippet.objects.all()
 	serializers_class = SnippetSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly)
 
 class UserList(generics.ListAPIView):
 	queryset = User.objects.all()
