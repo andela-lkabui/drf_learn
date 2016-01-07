fa17c435863ba5ece023bba02398fd4f0d37e971
Lewis Maina Kabui <lewis_maina@yahoo.com>
2016-01-07 21:54:55 +0300

    Add rest framework login url and snippets.urls

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,6 +17,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^', include('snippets.urls')),
     url(r'^admin/', admin.site.urls),
-    #url(r'^', include('snippets.urls')),
+]
+
+urlpatterns += [
+    url(
+    	r'^api-auth/',
+    	include(
+    		'rest_framework.urls',
+    		namespace='rest_framework'
+    	)
+    ),
 ]
